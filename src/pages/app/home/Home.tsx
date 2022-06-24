74da6da1b4f89d4cbd61c2977ee73bfad74a86f1
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-24 12:35:59 +0300

    Home Pages added

---
+++ src/pages/app/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../../components/app/footer/Footer';
+import Navi from '../../../components/app/navi/Navi';
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Navi />
+      <h1 >Home Page</h1>
+    </div>
+
+  )
+}
