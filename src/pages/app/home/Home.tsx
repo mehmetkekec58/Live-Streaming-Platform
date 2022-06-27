878fd332e5fe1fd785fd9b61be624fa0348e4694
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-27 23:56:28 +0300

    fixed scrollbar

--- src/pages/app/home/Home.tsx
+++ src/pages/app/home/Home.tsx
@@ -10,11 +10,8 @@ import "./Home.css";
 
 export default function Home() {
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(0),
     textAlign: 'center',
-    color: theme.palette.text.secondary,
+   
   }));
   return (
     <div className='app'>
