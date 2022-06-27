e02b388ba8e7f1e22c70d178cf15196ec1be0074
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-27 23:53:55 +0300

    edited Drawer component and css file

--- src/components/app/drawer/Drawer.tsx
+++ src/components/app/drawer/Drawer.tsx
@@ -9,6 +9,9 @@ import "./Drawer.css";
 import Footer from '../footer/Footer';
 import MyAvatarComponent from '../../../utilities/avatar-component/MyAvatarComponent';
 import { numberRounder } from '../../../helper/numberRounder';
+import { Divider, ImageListItem, ListSubheader } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import TrendingIcon from '@mui/icons-material/TrendingUp';
 
 const data = [
   {
@@ -22,34 +25,127 @@ const data = [
   {
     fullName: "Ceren Aksal",
     viewers: 852,
-  }
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 1500000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 1241112,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 15000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 2351,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 15000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 2351,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 15000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 2351,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 15000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 2351,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
+  {
+    fullName: "Murat kiraz",
+    viewers: 15000,
+  },
+  {
+    fullName: "Deniz Bozdağ",
+    viewers: 2351,
+  },
+  {
+    fullName: "Ceren Aksal",
+    viewers: 852,
+  },
 ]
+function screenHeight():number {
+  return screen.height;
+  
+}
 
 function Drawer() {
   return (
     <div className='drawer-div'>
-      <Box sx={{ width: '100%', bgcolor: 'white' }}>
-        <nav aria-label="main">
+      <Box sx={{ bgcolor: 'white' }}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText  primary="Ana Sayfa" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <TrendingIcon />
+              </ListItemIcon>
+              <ListItemText primary="Trendler" />
+            </ListItemButton>
+          </ListItem>
+        </List>
           <List>
+            <Divider />
+            <span style={{ float: 'left', marginTop: '5px', marginLeft: '15px', fontSize: '20px', fontWeight: 'bold', fontFamily: 'Ubuntu' }}>Abonelikler</span>
             {data.map((e) => (
               <ListItem disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
-                   <MyAvatarComponent fullName={e.fullName} />
+                    <MyAvatarComponent fullName={e.fullName} />
                   </ListItemIcon>
                   <ListItemText primary={e.fullName} />
                   <div className=''>
-                    <div className='circle'></div>
-                    <p>{numberRounder(e.viewers)}</p>
+                    <div className='circle-green'></div>
+                    <p style={{float:'right', marginLeft:'5px'}}>{numberRounder(e.viewers)}</p>
                   </div>
                 </ListItemButton>
               </ListItem>
-
             ))}
-
           </List>
-        </nav>
-
+          <Divider/>
+    <Footer/>
       </Box>
     </div>
 
