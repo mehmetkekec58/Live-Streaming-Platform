ced0923528e5bbbe2b439f0137adfb868c88769a
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-27 23:53:07 +0300

    more colors added

--- src/contains/colors.ts
+++ src/contains/colors.ts
@@ -1,1 +1,1 @@
-export const COLOR_LIST: string[] = ["red", "blue", "green", "purple", "pink", "orange", "brown"];
+export const COLOR_LIST: string[] = ["red", "blue", "green", "purple", "pink", "orange", "brown","yellow","darkblue","grey","black"];
