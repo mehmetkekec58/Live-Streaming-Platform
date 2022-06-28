215092b32b919d18c1412eca45b2b99d25c194ea
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-28 18:05:52 +0300

    Fixed Footer

--- src/components/app/footer/Footer.tsx
+++ src/components/app/footer/Footer.tsx
@@ -10,50 +10,49 @@ import HomeIcon from '@mui/icons-material/Home';
 import TrendingIcon from '@mui/icons-material/TrendingUp';
 import { Divider } from '@mui/material';
 
-const data =[
+const data = [
   {
-   text:"Gizlilik Sözleşmesi",
-   link:"#",
+    text: "Gizlilik Sözleşmesi",
+    link: "#",
   },
   {
-    text:"Çerezler",
-    link:"#",
-   },
-   {
-    text:"Reklam Ver",
-    link:"#",
-   },
+    text: "Çerezler",
+    link: "#",
+  },
+  {
+    text: "Reklam Ver",
+    link: "#",
+  },
 
-   {
-    text:"Bize Ulaşın",
-    link:"#",
-   },
-   {
-    text:"Geri Bildirim",
-    link:"#",
-   },
+  {
+    text: "Bize Ulaşın",
+    link: "#",
+  },
+  {
+    text: "Geri Bildirim",
+    link: "#",
+  },
 
 ]
 
 function Footer() {
   return (
-    <footer>  
-        <Box sx={{ bgcolor: 'white' }}>
-    <List>
-      {data.map((e) =>(
+    <footer>
+      <Box sx={{ bgcolor: 'white', textAlign: 'center' }}>
+        <List>
+          {data.map((e) => (
 
-<ListItem disablePadding>
-<ListItemButton>
-  <ListItemText  primary={e.text} />
-</ListItemButton>
-</ListItem>
-      ))}
-     
-  </List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary={e.text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+
+        </List>
       </Box>
-     
-      <span style={{fontSize:'12px'}}>Canlı Yayın Platformu 2022</span>
-      </footer>
+      <span style={{ fontSize: '12px' }}> © 2022 Canlı Yayın Platformu</span>
+    </footer>
   )
 }
 
