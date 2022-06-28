2e254fe48a542ed06e53cb60741ed0ae0e481d24
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-28 21:11:13 +0300

    search router added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/app/home/Home';
+import Navi from './components/app/navi/Navi';
+import Search from './pages/app/search/Search';
+
 
 function App() {
   return (
+    <div>
+    <Navi/>
     <Routes>
-      <Route path='' element={<Home />} />
+      <Route path='' element={<Home />} /> 
+      <Route path='search' element={<Search/>} />
     </Routes>
+    </div>
   );
 }
 
