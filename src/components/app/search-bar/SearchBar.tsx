d7b44fbddefb9709f4cbc306f7620a5a997a0389
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-28 21:11:50 +0300

    edited search bar

--- src/components/app/search-bar/SearchBar.tsx
+++ src/components/app/search-bar/SearchBar.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/esm/Button';
 import FormControl from 'react-bootstrap/esm/FormControl';
 import InputGroup from 'react-bootstrap/esm/InputGroup';
 import SearchIcon from '@mui/icons-material/Search';
 import './SearchBar.css';
+import { Link } from 'react-router-dom';
+
 
 
 function SearchBar() {
+    const [search, setSearch] = useState<string>("");
+
+    const OnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
     return (
         <div className='search'>
             <InputGroup>
                 <FormControl
                     placeholder="Ara"
                     as='input'
+                    onChange={OnChange}
                 />
-                <Button className='button'>
-                    <SearchIcon />
-                </Button>
+                {search != "" && search != null ?
+                    <Link to={`/search?q=${search}`}>
+                        <Button variant='light' className='button'>
+                            <SearchIcon />
+                        </Button>
+                    </Link>
+                    :
+                    ""
+                }
             </InputGroup>
         </div>
     )
