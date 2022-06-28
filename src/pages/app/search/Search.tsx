da0610f39e5fd50e7efb8a952e52346b6603f3a9
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-28 23:47:06 +0300

    edited search page

--- src/pages/app/search/Search.tsx
+++ src/pages/app/search/Search.tsx
@@ -1,14 +1,38 @@
+import Grid from '@mui/material/Grid/Grid';
+import Paper from '@mui/material/Paper/Paper';
+import styled from '@mui/material/styles/styled';
 import React from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
+import Drawer from '../../../components/app/drawer/Drawer';
+import SearchList from '../../../components/app/search-list/SearchList';
 import './Search.css';
 
 
 function Search() {
  const [searchParams, getSearchParams] = useSearchParams();
-  
+ 
+
+  const Item = styled(Paper)(({ theme }) => ({
+    textAlign: 'center',
+
+  }));
+
   return (
-    <div>hey {searchParams.get("q")}</div>
+    <div className='search-page'>
+      {/* hey {searchParams.get("q")} */}
+      <Grid container columns={16}>
+      <Grid item xs={3}>
+        <Item ><Drawer /></Item>
+      </Grid>
+      <Grid item xs={13}>
+        <Item><SearchList/></Item>
+      </Grid>
+    </Grid>
+      
+    
+    </div>
   )
+
 }
 
 export default Search
