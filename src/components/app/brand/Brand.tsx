1569f9ea67305ccb07cf0ea9a5c4f6ca9ffba224
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-28 18:04:12 +0300

    fixed brand component and add button

--- src/components/app/brand/Brand.tsx
+++ src/components/app/brand/Brand.tsx
@@ -1,21 +1,24 @@
+import { Button } from '@mui/material';
 import React from 'react'
 import "./Brand.css";
+import HomeIcon from '@mui/icons-material/Menu';
 
 let logo: string = "https://icon-library.com/images/icon-png-logos/icon-png-logos-7.jpg";
 let brandName: string = "Canlı Yayın Platformu";
 
 function Brand() {
   return (
-    <div style={{ float: 'left' }}>
-    <img
-      alt=""
-      src={logo}
-      width="40"
-      height="40"
-      className="d-inline-block"
-    />{'   '}
-    <text className='brand-name'>{brandName}</text>
-  </div>
+    <div className="brand">
+      <Button className='button'><HomeIcon style={{ color: 'grey' }} /></Button>
+      <img
+        alt="Canlı Yayın Platformu"
+        src={logo}
+        width="40"
+        height="40"
+        className="d-inline-block brand-img"
+      />{'   '}
+      <text className='brand-name'>{brandName}</text>
+    </div>
   )
 }
 
