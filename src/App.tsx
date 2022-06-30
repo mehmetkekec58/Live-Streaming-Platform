0f0eef3418689dc243c7371373677c4f38ef6e53
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-30 23:35:56 +0300

    fixed app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/app/home/Home';
 import Navi from './components/app/navi/Navi';
 import Search from './pages/app/search/Search';
+import Grid from '@mui/material/Grid/Grid';
+import styled from '@mui/material/styles/styled';
+import Paper from '@mui/material/Paper/Paper';
+import Drawer from './components/app/drawer/Drawer';
 
+const Item = styled(Paper)(({ theme }) => ({
+  textAlign: 'center',
+
+}));
 
 function App() {
+
+  const [drawerClose, setDrawerClose] = useState<boolean>(false)
+
   return (
     <div>
     <Navi/>
-    <Routes>
+
+
+    <Grid container columns={16}>
+      <Grid item xs={3}>
+        <Item ><Drawer /></Item>
+      </Grid>
+      <Grid item xs={13}>
+        <Item>
+          <Routes>
       <Route path='' element={<Home />} /> 
       <Route path='search' element={<Search/>} />
     </Routes>
+    </Item>
+      </Grid>
+    </Grid>
+
+   
     </div>
   );
 }
