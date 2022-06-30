e9bd240d03d973a69a30612b96a89bccf5b10539
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-06-30 23:39:16 +0300

    fixed Home page

--- src/pages/app/home/Home.tsx
+++ src/pages/app/home/Home.tsx
@@ -8,50 +8,11 @@ import Main from '../../../components/app/main/Main';
 import Navi from '../../../components/app/navi/Navi';
 import "./Home.css";
 
-function name(): JSX.Element {
-  const Item = styled(Paper)(({ theme }) => ({
-    textAlign: 'center',
 
-  }));
-  return (
-    <Grid container columns={16}>
-      <Grid item xs={3}>
-        <Item ><Drawer /></Item>
-      </Grid>
-      <Grid item xs={13}>
-        <Item><Main /></Item>
-      </Grid>
-    </Grid>
-  )
-}
-function name1(): JSX.Element {
-  const Item = styled(Paper)(({ theme }) => ({
-    textAlign: 'center',
-
-  }));
-  return (
-    <Grid container columns={16}>
-      <Grid item xs={16}>
-        <Item><Main /></Item>
-      </Grid>
-    </Grid>
-  )
-}
 export default function Home() {
-  const Item = styled(Paper)(({ theme }) => ({
-    textAlign: 'center',
-
-  }));
-
-  const [drawerClose, setDrawerClose] = useState<boolean>(false)
-
   return (
     <div className='app'>
-      
-      <Box sx={{ flexGrow: 1 }}>
-        {drawerClose == false ? name() : name1()}
-      </Box>
-
+      <Main />
     </div>
 
   )
