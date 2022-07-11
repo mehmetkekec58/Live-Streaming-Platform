eb75a773a935b94fc25b2e6818990d99f5be1ae4
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-11 23:44:22 +0300

    updated video description component to try

--- src/components/app/video-description/VideoDescription.tsx
+++ src/components/app/video-description/VideoDescription.tsx
@@ -1,9 +1,17 @@
+import { Divider } from '@mui/material';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import'./VideoDescription.css';
 
 function VideoDescription() {
   return (
-    <div>VideoDescription</div>
+    <div>
+      <span className="video-description-video-title">Valorant Canlı Yayını</span>
+      <Divider/>
+      <span>İzleyici: 14 B</span>
+      <Button variant='warning' className='video-description-follow-button'>Takip Et</Button>
+      <span>Merhaba ben Nagehan Uzun hergün canlı yayın açıyorum. Burada FPS oyunları oyunlarının neredeyse çoğunu oynuyorum.</span>
+      </div>
   )
 }
 
